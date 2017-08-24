de43923bd73427c2484f78642f01e4a1149096ed
Gennaro Anzalone, Kelsey Langlois and linda luu <gennaro@>
2017-08-24 09:02:11 -0700

    add three-in-a-row function

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,38 @@ Space.prototype.addMark = function(newMark) {
   }
 }
 
+function Board() {
+  this.spaces = [];
+  for (var i = 0; i < 3; i++) {
+    for(var j = 0; j < 3; j ++) {
+      this.spaces.push(new Space(i,j));
+    }
+  }
+}
+
+Board.prototype.threeInRow = function() {
+  if (this.spaces[0].getMark() === this.spaces[1].getMark() && this.spaces[1].getMark() === this.spaces[2].getMark() && this.spaces[0].getMark() !== ""){
+    return true;
+  } else if(this.spaces[3].getMark() === this.spaces[4].getMark() && this.spaces[4].getMark() === this.spaces[5].getMark()&& this.spaces[3].getMark() !== ""){
+    return true;
+  } else if (this.spaces[6].getMark() === this.spaces[7].getMark() && this.spaces[7].getMark() === this.spaces[8].getMark()&& this.spaces[6].getMark() !== "") {
+    return true;
+  } else if (this.spaces[0].getMark() === this.spaces[3].getMark() && this.spaces[6].getMark() === this.spaces[0].getMark()&& this.spaces[0].getMark() !== "") {
+    return true;
+  } else if (this.spaces[1].getMark() === this.spaces[4].getMark() && this.spaces[7].getMark() === this.spaces[4].getMark()&& this.spaces[1].getMark() !== "") {
+    return true;
+  } else if (this.spaces[2].getMark() === this.spaces[5].getMark() && this.spaces[8].getMark() === this.spaces[2].getMark()&& this.spaces[2].getMark() !== "") {
+    return true;
+  } else if (this.spaces[0].getMark() === this.spaces[4].getMark() && this.spaces[8].getMark() === this.spaces[0].getMark()&& this.spaces[0].getMark() !== "") {
+    return true;
+  } else if (this.spaces[2].getMark() === this.spaces[4].getMark() && this.spaces[6].getMark() === this.spaces[2].getMark()&& this.spaces[2].getMark() !== "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 // UI Logic
 $(document).ready(function() {
   var player1 = new Player("X");
@@ -33,8 +65,11 @@ $(document).ready(function() {
 
   var space1 = new Space(1,1);
   console.log(space1);
-  console.log(space1.getMark());
-  console.log(space1.addMark("O"));
-  console.log(space1.getMark());
-  console.log(space1.addMark("X"));
+
+  var board1 = new Board();
+  console.log(board1);
+
+  console.log(board1.threeInRow());
+
+
 });
